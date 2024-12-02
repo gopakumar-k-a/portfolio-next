8d441ee1d6bab75585038e967705f11dbb6f24b4
gopakumar-k-a <gopak9145@gmail.com>
2024-12-02 14:06:51 +0530

    updated resume

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const DownloadResume = ({ children }: { children: React.ReactNode }) => {
   return (
     <a
-      href="/resume.pdf"
-      download="Gopakumar_Resume.pdf"
+      href="/GOPAKUMAR_KA-MERN_STACK_DEVELOPER.pdf"
+      download="GOPAKUMAR_KA-MERN_STACK_DEVELOPER.pdf"
     >
       {children}
     </a>
